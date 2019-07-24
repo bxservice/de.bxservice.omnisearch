c3068aa0937df8e53f080d7e98e31e4c4c2a34a7
Diego Ruiz <12065321+d-ruiz@users.noreply.github.com>
2019-07-24 20:42:55 +0200

    Restart event validator when a table text search index flag is modified

--- de.bxservice.omnisearch_tsearch_pg/src/de/bxservice/omnisearch/validator/TSearchIndexEventHandler.java
+++ de.bxservice.omnisearch_tsearch_pg/src/de/bxservice/omnisearch/validator/TSearchIndexEventHandler.java
@@ -25,34 +25,39 @@ import java.util.List;
 import java.util.logging.Level;
 
 import org.adempiere.base.event.AbstractEventHandler;
+import org.adempiere.base.event.IEventManager;
 import org.adempiere.base.event.IEventTopics;
+import org.compiere.model.MColumn;
 import org.compiere.model.PO;
 import org.compiere.util.CLogger;
-import org.osgi.service.component.annotations.Component;
 import org.osgi.service.event.Event;
-import org.osgi.service.event.EventHandler;
 
 import de.bxservice.omniimpl.TextSearchValues;
 import de.bxservice.omnisearch.tools.OmnisearchAbstractFactory;
 import de.bxservice.omnisearch.tools.OmnisearchHelper;
 
-@Component(
-		service = EventHandler.class
-)
 public class TSearchIndexEventHandler extends AbstractEventHandler {
 
 	/**	Logger			*/
 	private static CLogger log = CLogger.getCLogger(TSearchIndexEventHandler.class);
+	
+	private String trxName = null;
 
 	@Override
 	protected void initialize() {
-		List<String> indexedTables = OmnisearchHelper.getIndexedTables(TextSearchValues.TS_INDEX_NAME);
-		
+		log.warning("");
+
+		List<String> indexedTables = OmnisearchHelper.getIndexedTables(TextSearchValues.TS_INDEX_NAME, trxName);
+
 		for (String tableName : indexedTables) {
 			registerTableEvent(IEventTopics.PO_AFTER_NEW, tableName);
 			registerTableEvent(IEventTopics.PO_AFTER_CHANGE, tableName);
 			registerTableEvent(IEventTopics.PO_AFTER_DELETE, tableName);
 		}
+		
+		registerTableEvent(IEventTopics.PO_AFTER_NEW, MColumn.Table_Name);
+		registerTableEvent(IEventTopics.PO_AFTER_CHANGE, MColumn.Table_Name);
+		registerTableEvent(IEventTopics.PO_AFTER_DELETE, MColumn.Table_Name);
 	}
 
 	@Override
@@ -64,9 +69,21 @@ public class TSearchIndexEventHandler extends AbstractEventHandler {
 				|| type.equals(IEventTopics.PO_AFTER_DELETE)) {
 
 			PO po = getPO(event);
-			if (log.isLoggable(Level.INFO)) 
+			if (po instanceof MColumn) {
+				if ((type.equals(IEventTopics.PO_AFTER_CHANGE) &&
+						po.is_ValueChanged(TextSearchValues.TS_INDEX_NAME)) || 
+						(po.get_ValueAsBoolean(TextSearchValues.TS_INDEX_NAME))) {
+					//If the Text search index flag is changed -> register/unregister the modified table
+					trxName = po.get_TrxName();
+					IEventManager tempManager = eventManager;
+					unbindEventManager(eventManager);
+					bindEventManager(tempManager);
+				}
+			}
+
+			if (log.isLoggable(Level.INFO))
 				log.info("Recreating index po : " + po + "type: "+type);
-			
+
 			OmnisearchHelper.recreateIndex(OmnisearchAbstractFactory.TEXTSEARCH_INDEX);
 		}
 	}
